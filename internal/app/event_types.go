18aaf3bd4a738ae79b171d72080c0f07ddb2c281
Michael <mvasilyev04@gmail.com>
2026-01-26 20:18:18 +0300

    Done method Execute to AcceptEvent

--- internal/app/event_types.go
+++ internal/app/event_types.go
@@ -1,5 +1,10 @@
 package app
 
+import (
+	"NotificationService/internal/domain"
+	"context"
+)
+
 type AcceptEvent struct {
 	IdemStore IdempotencyStore
 	Repo      EventRepository
@@ -10,4 +15,32 @@ type ProcessEvent struct {
 	Repo      EventRepository
 	Queue     EventQueue
 	Publisher NotificationSender
+}
+
+func (ae *AcceptEvent) Execute(key string, payload any) error {
+	ok, err := ae.IdemStore.CheckKey(context.Background(), key)
+	if err != nil {
+		return err
+	}
+	if ok {
+		return nil
+	}
+	event := domain.NewEvent(key, payload)
+	event.Accept()
+	err = ae.IdemStore.SaveKey(context.Background(), key)
+	if err != nil {
+		event.Reject()
+		return err
+	}
+	err = ae.Repo.SaveEvent(context.Background(), event)
+	if err != nil {
+		event.Reject()
+		return err
+	}
+	err = ae.Publisher.PublishEvent(context.Background(), event)
+	if err != nil {
+		event.Reject()
+		return err
+	}
+	return nil
 }
