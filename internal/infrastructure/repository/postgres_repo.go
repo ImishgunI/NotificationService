9e91ffdf1a440f22b3be153a3946170ca531b372
Michael <mvasilyev04@gmail.com>
2026-02-06 19:00:10 +0300

    done repository

--- internal/infrastructure/repository/postgres_repo.go
+++ internal/infrastructure/repository/postgres_repo.go
@@ -1,1 +1,67 @@
 package repository
+
+import (
+	"NotificationService/internal/domain"
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Repository struct {
+	pool *pgxpool.Pool
+}
+
+func NewPoolPG(ctx context.Context, url string) (*Repository, error) {
+	pool, err := pgxpool.New(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+	return &Repository{
+		pool: pool,
+	}, nil
+}
+
+func (p *Repository) CloseDB() {
+	p.pool.Close()
+}
+
+func (p *Repository) SaveEvent(ctx context.Context, e *domain.Event) error {
+	_, err := p.pool.Query(ctx,
+		`INSERT INTO events (key, status, payload) VALUES ($1, $2, $3)`,
+		e.GetKey(), e.GetStatus(), e.GetPayload())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Repository) GetEvent(ctx context.Context, key string) (domain.Event, error) {
+	e := domain.Event{}
+	var (
+		status  domain.EventStatus
+		payload any
+	)
+	err := p.pool.QueryRow(ctx, `
+		SELECT status, payload FROM events
+		WHERE key = $1
+	`, key).Scan(&status, &payload)
+	if err != nil {
+		return domain.Event{}, err
+	}
+	e.SetKey(key)
+	e.SetStatus(status)
+	e.SetPayload(payload)
+	return e, nil
+}
+
+func (p *Repository) UpdateEventStatus(ctx context.Context, eventStatus domain.EventStatus, key string) error {
+	_, err := p.pool.Query(ctx, `
+		UPDATE events
+		SET status = $1
+		WHERE key = $2
+	`, eventStatus, key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
