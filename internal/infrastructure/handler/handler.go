f102ebe0603092017860bf7007cefcddecd57920
Michael <mvasilyev04@gmail.com>
2026-02-13 17:35:52 +0300

    create handler

--- internal/infrastructure/handler/handler.go
+++ internal/infrastructure/handler/handler.go
@@ -1,4 +1,30 @@
 package handler
 
-type Handler struct {
+import (
+	"NotificationService/internal/domain"
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type BaseEvent struct {
+	Type string `json:"type"`
+}
+
+type Dispatcher struct{}
+
+func (d *Dispatcher) Handle(ctx context.Context, payload json.RawMessage) error {
+	var base BaseEvent
+	if err := json.Unmarshal(payload, &base); err != nil {
+		return err
+	}
+	switch base.Type {
+	case "send_email":
+		break
+	case "create_order":
+		break
+	default:
+		return fmt.Errorf("%w", domain.ErrUnknownEventType)
+	}
+	return nil
 }
